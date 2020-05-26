ac190c3856eb861f3f16c0cf376aca551822f455
jingxin <1120095173@qq.com>
2020-05-26 10:13:27 +0800

    +）首页；+）新闻页

---
+++ pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		<el-button>默认按钮</el-button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
